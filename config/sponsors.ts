c10b0b8d28ad31820709560d25e8e516f495c90c
Aaron Powell <me@aaron-powell.com>
2018-05-23 09:59:37 +1000

    adding our sponsors

--- config/sponsors.ts
+++ config/sponsors.ts
@@ -1,5 +1,20 @@
-import { Sponsor } from './types'
+import { Sponsor, SponsorType } from './types'
 
-const Sponsors: Sponsor[] = []
+const Sponsors: Sponsor[] = [{
+  imageUrl: '/static/images/sponsors/ndc-sydney.png',
+  name: 'NDC Sydney',
+  type: SponsorType.Silver,
+  url: 'https://ndcsydney.com'
+}, {
+  imageUrl: '/static/images/sponsors/readify.png',
+  name: 'Readify & Telstra BTS',
+  type: SponsorType.Gold,
+  url: 'https://readify.net'
+}, {
+  imageUrl: '/static/images/sponsors/aspose.png',
+  name: 'Aspose',
+  type: SponsorType.Standard,
+  url: 'http://aspose.com/',
+}]
 
 export default Sponsors

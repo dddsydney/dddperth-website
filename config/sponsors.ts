53e094a755ae3d8fd429aa25f0714d62dae9fe9f
raph <me@raph.ws>
2019-08-12 16:29:51 +1000

    Add YOW!

--- config/sponsors.ts
+++ config/sponsors.ts
@@ -31,49 +31,13 @@ const Sponsors: Sponsor[] = [
     type: SponsorType.Community,
     url: 'https://musescodejs.org/sydney.html',
   },
-  // {
-  //   imageUrl: '/static/images/sponsors/aspose.png',
-  //   name: 'Aspose',
-  //   type: SponsorType.Standard,
-  //   url: 'http://aspose.com/',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/devexpress.png',
-  //   name: 'DevExpress',
-  //   type: SponsorType.Silver,
-  //   url: 'https://devexpress.com',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/2018-Progress_Telerik.png',
-  //   name: 'Progress',
-  //   type: SponsorType.Gold,
-  //   url: 'https://www.progress.com/',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/microsoft.png',
-  //   name: 'Microsoft',
-  //   type: SponsorType.Gold,
-  //   url: 'https://www.microsoft.com/en-au',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/ssw.png',
-  //   name: 'SSW TV',
-  //   serviceProvided: 'Media Partner',
-  //   type: SponsorType.Service,
-  //   url: 'https://ssw.tv',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/2018-lendi.png',
-  //   name: 'Lendi',
-  //   type: SponsorType.Gold,
-  //   url: 'https://www.lendi.com.au',
-  // },
-  // {
-  //   imageUrl: '/static/images/sponsors/2018-nib.png',
-  //   name: 'nib',
-  //   type: SponsorType.Standard,
-  //   url: 'https://www.nib.com.au',
-  // },
+  {
+    imageUrl: '/static/images/sponsors/yow.png',
+    name: 'YOW!',
+    type: SponsorType.Service,
+    url: 'https://www.yowconference.com/',
+    serviceProvided: "Lock Note Speaker"
+  }
 ]
 
 export default Sponsors
